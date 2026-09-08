d262ebe96bc9d713019336822a711c62365bc36f
David Glymph <Woozl@users.noreply.github.com>
2026-09-07 23:43:39 -0400

    busy loop blink

--- 02-busy-loop-blink/README.md
+++ 02-busy-loop-blink/README.md
@@ -1,5 +1,14 @@
 # 02 - Busy Loop Blink
 
+This project takes the busy loop delay code from the previous
+project and adds some GPIO code to make an LED blink. I've also
+checked the output frequency to confirm it is roughly 0.5Hz,
+matching the expected 1 second on, 1 second off behavior.
+
+![Pin PB4 scope output](./pin4-output.png)
+
+## GPIO configuration reference:
+
 ![PORTB registers](./portb-registers.png)
 
 ![PORTB Pin configuration overview](./portb-pin-configuration.png)
--- 02-busy-loop-blink/busy-loop-blink.s
+++ 02-busy-loop-blink/busy-loop-blink.s
@@ -1,4 +1,61 @@
+; I have an LED's anode connected to pin PB4
+
+; these .set directives are just like macros:
+; .set <symbol>, <expression>
+.set PINB, 0x16
+.set DDRB, 0x17
+.set PORTB, 0x18
+
 .section .text
 .org 0x0000
 
+; First we need to write 1 to bit 4 (DDB4) of the DDRB register
+; to set the direction as output
+sbi DDRB 4
 
+; Then, we could toggle the pin on (1) and off (0) by setting the
+; PORTB4 bit in the PORTB register
+
+; Turning the pin on:
+; sbi PORTB, 4
+
+; Turning the pin off:
+; cbi PORTB, 4
+
+
+; ======= new instructions ======= ;
+
+; sbi - set bit in io register
+; sbi A, b
+; sets the bth bit of io address A to 1
+
+; cbi - clear bit in io register
+; cbi A, b
+; clears the bth bit of io address A to 0
+
+; ================================ ;
+
+
+
+loop:
+  ; instead, if we write a 1 to the bit in the pin input register,
+  ; it toggles the PORTB4put of the pin. This only works when the pin
+  ; is in output mode.
+  sbi PINB, 4
+  rjmp delay ; noop
+
+; our delay code from the last project
+delay:
+  ldi r18, 42
+  outer:
+    ldi r17, 31
+    middle:
+      ldi r16, 255
+      inner:
+        dec r16
+        brne inner
+      dec r17
+      brne middle
+    dec r18
+    brne outer
+rjmp loop
